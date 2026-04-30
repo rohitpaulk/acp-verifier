e44e21fc03108f986c7392f631a0c589916a4851
Paul Kuruvilla <rohitpaulk@gmail.com>
2026-04-30 19:20:23 +0530

    Update test setup to use dynamic imports and change result statuses to pass.

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,10 +1,13 @@
 import { setDefaultTimeout, afterAll } from "bun:test";
 import chalk from "chalk";
-import { AgentRegistry } from "../lib/agent-registry";
-import { CheckCollectorRegistry } from "../lib/check-collector-registry";
-import { writeResults } from "../lib/results-writer";
 
 setDefaultTimeout(15_000);
+
+await import("../scripts/generate-check-slugs");
+
+const { AgentRegistry } = await import("../lib/agent-registry");
+const { CheckCollectorRegistry } = await import("../lib/check-collector-registry");
+const { writeResults } = await import("../lib/results-writer");
 
 export const registry = new AgentRegistry();
 await registry.buildAllImages();
@@ -42,4 +45,3 @@ afterAll(() => {
 
   writeResults(checkCollectorRegistry);
 });
-
